43525a220e9a13e8f9ce9165facf2c43a039bed1
Soroush <soroush.shahhoseini@gmail.com>
2016-05-25 14:44:29 +0430

    gitignore file added

---
+++ zoodfood-marpelle/zoodfood-marpelle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zoodfood-marpelle
+//
+//  Created by Soroush Shahhoseini on 3/5/1395 AP.
+//  Copyright © 1395 AP Shahho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
